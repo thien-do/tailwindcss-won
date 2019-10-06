b4cbc769a4a4e7f46eb51625a68df50cc97b9596
dvkndn <dvkndn@gmail.com>
2019-10-06 14:57:51 +0700

    remove normalize css

--- src/plugins/preflight.js
+++ src/plugins/preflight.js
@@ -3,7 +3,7 @@ import postcss from 'postcss'
 
 export default function() {
   return function({ addBase }) {
-    const normalizeStyles = postcss.parse(fs.readFileSync(require.resolve('normalize.css'), 'utf8'))
+    const normalizeStyles = {}
     const preflightStyles = postcss.parse(fs.readFileSync(`${__dirname}/css/preflight.css`, 'utf8'))
     addBase([...normalizeStyles.nodes, ...preflightStyles.nodes])
   }
